68cec644e7dc03bd97c3f68a8b303da7e93d6e94
Belmiro42 <belmirocosta2805@gmail.com>
2024-08-01 14:45:02 +0200

    Nada

--- MINISHELL/MINISHELL/PARSING/4_Wildcard_Expansion.c
+++ MINISHELL/MINISHELL/PARSING/4_Wildcard_Expansion.c
@@ -6,7 +6,7 @@
 /*   By: bmatos-d <bmatos-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 03:01:41 by bmatos-d          #+#    #+#             */
-/*   Updated: 2024/08/01 14:05:17 by bmatos-d         ###   ########.fr       */
+/*   Updated: 2024/08/01 14:07:47 by bmatos-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,6 +151,6 @@ char *expand_wildcards(char *str)
 	}
 	//printf("Temp %d\tStart %d\tLen %d\n", temp, start, (int)strlen(str));
 	output = ft_strjoin(output, ft_substr(str, temp, strlen(str) - temp ), 1, 1);
-	printf("FINAL %s\n", output);
+	//printf("FINAL %s\n", output);
 	return(output);
 }
